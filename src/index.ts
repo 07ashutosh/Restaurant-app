3932e2fd71843d6db0b5c75e23a0642bdfe3c283
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-01-27 10:10:08 +0530

    add new routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { app } from "./app"
+import { app } from "./app";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB";
 import express from "express";
-import userRouter from "./routes/user.routes";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import bodyparser, { json } from "body-parser"
+import bodyparser, { json } from "body-parser";
+import userRouter from "./routes/user.routes";
+import restaurantRoute from "./routes/restaurant.routes";
+import menuRouter from "./routes/menu.routes";
 dotenv.config({path:"./.env"})
 
 const port: any = process.env.PORT || 5000;
@@ -24,6 +26,12 @@ app.use(cors(corsOption));
 
 app.use("/api/v1/user",userRouter);
 //http://localhost:5000/api/v1/user/signup
+
+app.use("/api/v1/restaurant",restaurantRoute);
+//http://localhost:5000/api/v1/restaurant/
+
+app.use("/api/v1/menu",menuRouter);
+//http://localhost:5000/api/v1/menu/
 
 connectDB()
 .then(()=>{
